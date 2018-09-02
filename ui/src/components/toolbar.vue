c88d87accaa53eb17d35eb71933e732745cabd99
RiteshGoyal <goyal.7@iitj.ac.in>
2018-09-02 19:21:01 +0530

    added genre and toolbar

---
+++ ui/src/components/toolbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+  <v-toolbar color="brown darken-1" id="toolbar">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title >Title</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>
+        <v-icon>account_circle</v-icon>
+        UserName
+        </v-btn>
+        <v-btn flat>
+        <v-icon>settings_power</v-icon>
+        logout</v-btn>
+        <v-btn flat>
+        <v-icon>error</v-icon>
+        About</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+
+export default{
+  name:'app',
+    data(){
+      return
+    }
+}
+</script>
+<style scoped>
+#toolbar{
+  color:black;
+}
+</style>
